707767f8e784affd5708fb20b0553bc741a45b53
Alisa Markaryan <alisa.markaryan@yahoo.com>
2021-02-02 15:53:45 +0400

    Initial commit of Homework

--- src/com/company/main1.java
+++ src/com/company/main1.java
@@ -4,23 +4,15 @@ import java.util.Scanner;
 
 public class main1 {
     public static void main(String[] args) {
-        Scanner x = new Scanner(System.in);
-        System.out.println("grel tvery");
-        int a = x.nextInt();
-        int b = x.nextInt();
-        Scanner y = new Scanner(System.in);
-        System.out.println("nshany");
-        char sign = y.next().charAt(0);
 
-        switch (sign) {
-            case '+':
-                System.out.println(a + b);
-                break;
-            case '-':
-                System.out.println(a - b);
-                break;
-            default:
-                System.out.println("NOt");
-        }
+        Scanner num = new Scanner(System.in);
+        System.out.println("enter the number ...");
+        int x = num.nextInt();
+        int y = ((1-x)*25)+(x*44);
+        System.out.println(y);
+
+
+
     }
-}
+
+    }
