234ec396cfe7a0ce0ac28249f453fc336d0dea1e
Alisa Markaryan <alisa.markaryan@yahoo.com>
2021-02-16 11:17:55 +0400

    Initial commit of homework

--- src/com/company/homework1.java
+++ src/com/company/homework1.java
@@ -2,11 +2,11 @@ package com.company;
 
 public class homework1 {
     public static void main (String[] args){
-        double[] arr = {7, 5, 3, 8, 2, 9};
+        int[] arr = {7, 5, 3, 8, 2, 9};
         System.out.println(average(arr));
     }
 
-    static double average(double[] arr) {
+    static double average(int[] arr) {
         double sum = 0;
         int i;
         for (i = 0; i < arr.length; i++) {
