76e3f15e74919eff58162cd05385cd810f05919e
Alisa Markaryan <alisa.markaryan@yahoo.com>
2021-02-27 14:58:08 +0400

    bro

--- src/com/company/homeworkp2c1.java
+++ src/com/company/homeworkp2c1.java
@@ -4,18 +4,35 @@ import java.util.Scanner;
 
 public class homeworkp2c1 {
     public static void main (String[] arg){
+//
+//        Scanner x = new Scanner(System.in);
+//        System.out.println("Enter the number...");
+//        int i=x.nextInt();
+//        System.out.println(func(i));
+//    }
+//    static int func(int i){
+//        if(i==0){
+//            return 0;
+//        }
+//        int sum=0;
+//        sum=sum+i%10;
+//        return sum+func(i/10);
 
-        Scanner x = new Scanner(System.in);
-        System.out.println("Enter the number...");
-        int i=x.nextInt();
-        System.out.println(func(i));
+        String s="barev";
+        System.out.println(func(s));
     }
-    static int func(int i){
-        if(i==0){
-            return 0;
+    static String func(String x){
+
+
+        char ch=x.charAt(x.length()-1);
+        if(x.length()-1==0){
+            return x;
         }
-        int sum=0;
-        sum=sum+i%10;
-        return sum+func(i/10);
+        System.out.print(ch);
+        return func(x.substring(0,x.length()-1));
+
+
+
+
     }
 }
