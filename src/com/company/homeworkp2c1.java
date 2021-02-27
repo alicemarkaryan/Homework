6fff9318126f195b0978bb589c6f9af6b0c2a4da
Alisa Markaryan <alisa.markaryan@yahoo.com>
2021-02-27 19:06:31 +0400

    polindrom

--- src/com/company/homeworkp2c1.java
+++ src/com/company/homeworkp2c1.java
@@ -3,36 +3,32 @@ package com.company;
 import java.util.Scanner;
 
 public class homeworkp2c1 {
-    public static void main (String[] arg){
-//
-//        Scanner x = new Scanner(System.in);
-//        System.out.println("Enter the number...");
-//        int i=x.nextInt();
-//        System.out.println(func(i));
-//    }
-//    static int func(int i){
-//        if(i==0){
-//            return 0;
-//        }
-//        int sum=0;
-//        sum=sum+i%10;
-//        return sum+func(i/10);
+    public static void main (String[] arg) {
 
-        String s="barev";
-        System.out.println(func(s));
+
+
+
+        String s="madam";
+   if(func(s)){
+    System.out.println("polindrom");
+}
+else{
+    System.out.println("non polindrom");
+
+}
     }
-    static String func(String x){
+     static boolean func(String x){
+        boolean f=true;
+      if(x.length()==1){
+      return true;
+       }
+  if(x.charAt(0)==x.charAt(x.length()-1)){
+      return func(x.substring(1,x.length()-1));
+  }
+  else {
+      return false;
+  }
+}
 
 
-        char ch=x.charAt(x.length()-1);
-        if(x.length()-1==0){
-            return x;
-        }
-        System.out.print(ch);
-        return func(x.substring(0,x.length()-1));
-
-
-
-
-    }
 }
