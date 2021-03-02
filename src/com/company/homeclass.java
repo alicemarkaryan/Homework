0e85e377aae90a4207c6ba4facebf793aebb8f14
Alisa Markaryan <alisa.markaryan@yahoo.com>
2021-03-02 14:49:37 +0400

    Homeworkwhy

--- src/com/company/homeclass.java
+++ src/com/company/homeclass.java
@@ -1,5 +1,41 @@
 package com.company;
 
+
+class Cup {
+    String form;
+    double height;
+    double radius;
+    int tail;
+
+    Cup(double r, double h, int t, String f) {
+        radius = r;
+        height = h;
+        tail = t;
+        form = f;
+    }
+    Cup(String f, double r){
+        form=f; radius=r;
+    }
+    Cup(String f, int tail){
+        form =f; this.tail=tail;
+    }
+}
+
+public class homeclass {
+    public static void main (String[] args){
+
+        Cup c1=new Cup(5.3,11.5,2,"round");
+        Cup c2=new Cup("square", 2.5);
+        Cup c3=new Cup("round", 1);
+        Cup c4 = c1;
+        c4.radius=5;
+        System.out.println(c1.radius);
+        System.out.println(c2.form+" "+c1.tail);
+        System.out.println(c3.form);
+        System.out.println(c3.tail+" "+c1.form);
+    }
+}
+
 class MercedesGLE{
     int year;
     double price;
@@ -18,7 +54,7 @@ class MercedesGLE{
         this.price=price; this.color=color; this.beauty=beauty;
     }
 }
-    public class homeclass{
+     class homeclass2{
     public static void main(String[] arg) {
         MercedesGLE coupe1 = new MercedesGLE(2020, 120000, 20000, 5.0, "Black", "Automatic",
                 "SUV / Truck");
