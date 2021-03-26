a876038b676a260c55a759ec8bb128760813a7a3
Alisa Markaryan <alisa.markaryan@yahoo.com>
2021-03-26 13:57:44 +0400

    Homework Generics

---
+++ src/com/company/MainFGeneric.java
@@ -0,0 +1,69 @@
+package com.company;
+
+public class MainFGeneric {
+
+
+    public static void main(String[] args) {
+
+
+        Airplane1 a1 = new Airplane1(2000);
+        Airplane1 a2 = new Airplane1(1500);
+        Airplane1 a3 = new Airplane1(3000);
+        Parrot1 p1 = new Parrot1(20);
+        Parrot1 p2 = new Parrot1(15);
+        Parrot1 p3 = new Parrot1(17);
+        Duck1 d1 = new Duck1(10);
+        Duck1 d2 = new Duck1(9);
+        Duck1 d3 = new Duck1(15);
+
+
+        Airplane1[] aa = {a1, a2, a3};
+        System.out.println(isIn(aa, a1));
+
+        Parrot1[] pp = {p1, p2,p3};
+        System.out.println(isIn(pp, p1));
+
+
+        Duck1[] dd = {d1, d2};
+        System.out.println(isIn(dd, d1));
+
+
+    }
+
+    private static  <T extends Flyable1<T>>  T isIn(T[] x, T y) {
+
+
+        for (int i = 0; i < x.length; i++)
+            if (y.equals(x[i])) {
+
+                return y;
+            }
+        return null;
+    }
+
+
+    private static boolean isIn(Parrot1[] pp, Parrot1 p) {
+        return true;
+    }
+
+    private static boolean isIn(Duck1[] dd, Duck1 d) {
+        return true;
+    }
+
+
+    private static boolean isIn(Airplane1[] aa, Airplane1 a1) {
+        return true;
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
+
